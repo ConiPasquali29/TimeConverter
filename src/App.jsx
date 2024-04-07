66b4e76b24be64487e469016793a0c23585ff3a8
ConiPasquali29 <cpasquali@frsn.utn.edu.ar>
2024-04-07 16:23:45 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import ConversorDeTiempo from "./components/conversorDeTiempo";
+
+function App() {
+  return (
+    <>
+      <ConversorDeTiempo />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/conversorDeTiempo.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import "./conversorDeTiempo.css";
+
+const ConversorDeTiempo = () => {
+  const [hour, setHour] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const [mode, setMode] = useState(false);
+
+  const handleHour = (value) => {
+    setHour(value);
+    setMinutes(value * 60);
+    setSeconds(value * 3600);
+  };
+
+  const handleMinutes = (value) => {
+    setHour(value / 60);
+    setMinutes(value);
+    setSeconds(value * 60);
+  };
+
+  const handleSeconds = (value) => {
+    setHour(value / 3600);
+    setMinutes(value / 60);
+    setSeconds(value);
+  };
+
+  const handleMode = () => {
+    setMode(!mode);
+  };
+
+  const containerStyle = {
+    backgroundColor: mode ? "#F2FDFF" : "#242424",
+    color: mode ? "#242424" : "#F2FDFF",
+    transition: "background-color 0.5s ease",
+  };
+
+  return (
+    <div
+      className={`container ${mode ? "container-transition" : ""}`}
+      style={{ ...containerStyle }}
+    >
+      <button onClick={handleMode}>{mode ? "Dia" : "Noche"}</button>
+      <h2>Conversor de Tiempo</h2>
+      <section className="time">
+        <label>Horas</label>
+        <input
+          type="number"
+          className="input"
+          value={hour}
+          onChange={({ target }) => handleHour(target.value)}
+        />
+      </section>
+      <section className="time">
+        <label>Minutos</label>
+        <input
+          type="number"
+          className="input"
+          value={minutes}
+          onChange={({ target }) => handleMinutes(target.value)}
+        />
+      </section>
+      <section className="time">
+        <label>Segundos</label>
+        <input
+          type="number"
+          className="input"
+          value={seconds}
+          onChange={({ target }) => handleSeconds(target.value)}
+        />
+      </section>
+    </div>
+  );
+};
+
+export default ConversorDeTiempo;
